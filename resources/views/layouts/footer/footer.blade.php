e0e09ec1241346a32c21f6d04a6b69eeccbcac9b
Nasser Esmlail <eng.naser2022@gmail.com>
2023-01-28 14:29:17 +0300

    footer

--- resources/views/layouts/footer/footer.blade.php
+++ resources/views/layouts/footer/footer.blade.php
@@ -63,7 +63,7 @@
                 </div>
                 <div class="col-md-6 text-center text-md-start">
                     <!--/*** This template is free as long as you keep the footer author’s credit link/attribution link/backlink. If you'd like to use the template without the footer author’s credit link/attribution link/backlink, you can purchase the Credit Removal License from "https://htmlcodex.com/credit-removal". Thank you for your support. ***/-->
-                   تصميم<a href="https://htmlcodex.com" >  مجموعة قوأب لتطوير البرمجيات </a>
+                   تصميم<a href="https://goupdev.net" >  فريق قوأب لتطوير البرمجيات </a>
                 </div>
             </div>
         </div>
